df8c307b474f1411427fa735bfd93d6c529d5e30
evateira <39007582+evateira@users.noreply.github.com>
2026-02-16 15:34:02 +0100

    first configuration project

---
+++ prueba-avoris/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'prueba-avoris';
+}
